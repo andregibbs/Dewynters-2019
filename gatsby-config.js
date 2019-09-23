c1785b4c63bf3bf62b5d4cc1c0dd1b0b96ffd745
Andre Gibbs <andregibbs@Digital-Studio.local>
2019-09-23 12:57:11 +0100

    updates careers page

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,34 +74,34 @@ module.exports = {
                 icon: `src/images/Favicon.jpg`, // This path is relative to the root of the site.
 			},
 		},
-        // {
-        //     resolve: `gatsby-plugin-google-analytics`,
-        //     options: {
-        //         trackingId: "UA-3161258-1",
-        //         // Puts tracking script in the head instead of the body
-        //         head: false,
-        //         // Setting this parameter is optional
-        //         // anonymize: true,
-        //         // Setting this parameter is also optional
-        //         // respectDNT: true,
-        //         // Avoids sending pageview hits from custom paths
-        //         // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        //         // Enables Google Optimize using your container Id
-        //         // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        //         // Enables Google Optimize Experiment ID
-        //         // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        //         // Set Variation ID. 0 for original 1,2,3....
-        //         // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        //         // Any additional create only fields (optional)
-        //         // sampleRate: 5,
-        //         // siteSpeedSampleRate: 10,
-        //         // cookieDomain: "example.com",
-        //     },
-        // },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-147287498-1",
+                // Puts tracking script in the head instead of the body
+                head: true,
+                // Setting this parameter is optional
+                // anonymize: true,
+                // Setting this parameter is also optional
+                // respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                // exclude: ["/preview/**", "/do-not-track/me/too/"],
+                // Enables Google Optimize using your container Id
+                // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+                // Enables Google Optimize Experiment ID
+                // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+                // Set Variation ID. 0 for original 1,2,3....
+                // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+                // Any additional create only fields (optional)
+                // sampleRate: 5,
+                // siteSpeedSampleRate: 10,
+                // cookieDomain: "example.com",
+            },
+        },
 		{
 			resolve: `gatsby-plugin-google-tagmanager`,
 			options: {
-				id: "GTM-NC55TTV",
+				id: "GTM-W46RC9",
 				includeInDevelopment: false,
 			},
 		},
